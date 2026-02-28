85ed64ecbb4180d1f00ac67310f656622032ca1b
Brenton Bostick <bostick@gmail.com>
2026-02-28 08:44:05 -0500

    ImGui fixes: make sure #endif comment matches the macro name

--- imgui.cpp
+++ imgui.cpp
@@ -16288,7 +16288,7 @@ static bool Platform_OpenInShellFn_DefaultImpl(ImGuiContext*, const char* path)
 #endif
 #else
 static bool Platform_OpenInShellFn_DefaultImpl(ImGuiContext*, const char*) { return false; }
-#endif // Default shell handlers
+#endif // IMGUI_DISABLE_DEFAULT_SHELL_FUNCTIONS
 
 //-----------------------------------------------------------------------------
 
